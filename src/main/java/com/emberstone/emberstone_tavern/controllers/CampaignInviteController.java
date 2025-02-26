e752e3c3c4d5997bbeb58b9edd3721c9607031a2
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-26 11:19:29 -0800

    refactor(invites): make API route plural (invites v invite)

--- src/main/java/com/emberstone/emberstone_tavern/controllers/CampaignInviteController.java
+++ src/main/java/com/emberstone/emberstone_tavern/controllers/CampaignInviteController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/campaigns/invite")
+@RequestMapping("api/campaigns/invites")
 public class CampaignInviteController {
     private final CampaignInviteService campaignInviteService;
 
